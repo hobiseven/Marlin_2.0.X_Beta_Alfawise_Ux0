09281d08627d9ce8194b92b6614eb4ef60b1e224
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2019-05-03 12:28:19 +0200

    safe accel. defaults and reduce jerk too

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -791,7 +791,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 400, 100, 3000 }
+#define DEFAULT_MAX_ACCELERATION      { 200, 200, 100, 3000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -801,9 +801,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          300    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION          200    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  500    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   350    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   200    // X, Y, Z acceleration for travel (non printing) moves
 
 //
 // Use Junction Deviation instead of traditional Jerk Limiting
@@ -822,8 +822,8 @@
  * value set here, it may happen instantaneously.
  */
 #if DISABLED(JUNCTION_DEVIATION)
-  #define DEFAULT_XJERK 20.0
-  #define DEFAULT_YJERK 20.0
+  #define DEFAULT_XJERK 10.0
+  #define DEFAULT_YJERK 10.0
   #define DEFAULT_ZJERK  0.4
 #endif
 
