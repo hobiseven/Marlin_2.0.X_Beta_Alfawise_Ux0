3a43448b2d38e8b7ca3d8fff5a726a190fe4ed8c
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2019-05-03 13:14:52 +0200

    change board number, conflicted now

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -244,7 +244,7 @@
 #define BOARD_GTM32_PRO_VB     1805   // STM32f103VET6 controller
 #define BOARD_MORPHEUS         1806   // STM32F103C8/STM32F103CB Libmaple based stm32f1 controller
 #define BOARD_MKS_ROBIN        1808   // MKS Robin / STM32F103ZET6
-#define BOARD_ALFAWISE_UX0     1809   // Alfawise U20/U20+/U30 / STM32F103VET6
+#define BOARD_ALFAWISE_UX0     1820   // Alfawise U20/U20+/U30 / STM32F103VET6
 
 //
 // STM32 ARM Cortex-M4F
